a799ab0699d37e5a3e250a3d89536f6ee0d4d706
Abhijeet Kashid <abhijeetkashid2002@gmail.com>
2026-02-07 02:04:21 +0530

    Supabase, connection, paper get and post api done

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import testRoutes from "./routes/test.routes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -9,10 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.json({ status: "API running 🚀" });
+  res.json({ status: "API running sucessfully." });
 });
 
 app.use("/api", routes);
+
+// app.use("/api/test", testRoutes);
 
 // error middleware last
 app.use(errorHandler);
--- api/src/controllers/papers.controller.js
+++ api/src/controllers/papers.controller.js
@@ -1,7 +1,55 @@
-export const getPapers = async (req, res) => {
-  res.json({ message: "Get papers endpoint" });
+import supabase from "../config/supabaseClient.js";
+import { uploadFileToStorage } from "../services/storage.service.js";
+
+export const uploadPaper = async (req, res, next) => {
+  try {
+    const { course, semester, subject, exam_session, year } = req.body;
+
+    if (!req.file) {
+      return res.status(400).json({ message: "File is required" });
+    }
+
+    // upload file to storage
+    const fileUrl = await uploadFileToStorage(req.file);
+
+    // save metadata in DB
+    const { data, error } = await supabase.from("papers").insert([
+      {
+        course,
+        semester,
+        subject,
+        exam_session,
+        year,
+        file_url: fileUrl,
+        approved: false,
+      },
+    ]);
+
+    if (error) throw error;
+
+    res.json({ message: "Paper uploaded successfully 🎉" });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const uploadPaper = async (req, res) => {
-  res.json({ message: "Upload paper endpoint" });
+export const getPapers = async (req, res, next) => {
+  try {
+    const { course, semester, subject } = req.query;
+
+    // let query = supabase.from("papers").select("*").eq("approved", true);
+    let query = supabase.from("papers").select("*")
+
+    if (course) query = query.eq("course", course);
+    if (semester) query = query.eq("semester", semester);
+    if (subject) query = query.eq("subject", subject);
+
+    const { data, error } = await query;
+
+    if (error) throw error;
+
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 };
